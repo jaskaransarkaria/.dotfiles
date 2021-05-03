dd4cefbf021f4b0f25ef6f77f6593bb07e30ded5
jaskaransarkaria <jaskaran.sarkaria@googlemail.com>
2021-05-03 22:02:07 +0100

    update volume script

--- scripts/get-volume.sh
+++ scripts/get-volume.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
-amixer get Master | tail -n1 | sed -r "s/.*\[(.*)%\].*/\1/"
+VOL_STATUS=$(amixer get Master | grep 'Right:' | awk -F'[][]' '{ print $4 }')
+VOL_VAL=$(amixer get Master | tail -n1 | sed -r "s/.*\[(.*)%\].*/\1/")
+
+case $VOL_STATUS in
+
+  on)
+    echo -n "🔉 $VOL_VAL"
+    ;;
+
+  *)
+    echo -n "🔇"
+    ;;
+esac
 
 exit 0
