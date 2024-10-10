85c26b4a2418445374262a5f51ac2cc1a39cf130
jaskaransarkaria <jaskaran.sarkaria@digital.justice.gov.uk>
2024-10-10 09:33:04 +0100

    feat: 🎸 finish dashboard

--- config/nvim/lua/plugins/dashboard.lua
+++ config/nvim/lua/plugins/dashboard.lua
@@ -2,34 +2,47 @@ return {
   'nvimdev/dashboard-nvim',
   event = 'VimEnter',
   dependencies = {
-    { 'MaximilianLloyd/ascii.nvim', { 'MunifTanjim/nui.nvim' }, { 'nvim-tree/nvim-web-devicons' } },
+    { 'MaximilianLloyd/ascii.nvim', { 'MunifTanjim/nui.nvim' }, { 'nvim-tree/nvim-web-devicons' }, { 'rubiin/fortune.nvim' } },
   },
   config = function()
+    local rand = require('ascii').get_random_global()
+    table.insert(rand, '')
+    table.insert(rand, '')
+    table.insert(rand, 'Jaskaran Sarkaria')
+    table.insert(rand, '')
     require('dashboard').setup {
+      shortcut_type = 'number',
+      hide = {
+        statusline = false,
+        tabline = false,
+      },
+      week_header = {
+        enable = true,
+      },
       config = {
-        header = require('ascii').get_random_global(),
+        header = rand,
         -- header = require('ascii').art.text.neovim.sharp,
         shortcut = {
           {
-            icon = ' ',
+            icon = '📂 ',
             desc = 'New',
             action = 'ene | startinsert',
             key = 'e',
           },
           {
-            icon = '󰍉 ',
+            icon = '🔎 ',
             desc = 'Find',
             action = 'Telescope find_files',
             key = 'f',
           },
           {
-            icon = ' ',
+            icon = '🗂️ ',
             desc = 'Grep',
             action = 'Telescope live_grep',
             key = 'g',
           },
           {
-            icon = ' ',
+            icon = '🌳 ',
             desc = 'Browse',
             action = 'NvimTreeFindFileToggle',
             key = 'e',
@@ -45,7 +58,16 @@ return {
           label = 'Recent Projects',
           action = 'Telescope find_files cwd=',
         },
-        footer = { '🚀 Sharp tools make good work.' },
+
+        footer = function()
+          local info = { '', '', '' }
+          local fortune_lib = require 'fortune'
+          fortune_lib.setup { content_type = 'mixed', display_format = 'mixed' }
+          local fortune = fortune_lib.get_fortune()
+          info[3] = '  Neovim loaded ' .. vim.fn.strftime '%H:%M' .. ' on ' .. vim.fn.strftime '%d/%m/%Y' .. " '"
+          local footer = vim.list_extend(info, fortune)
+          return footer
+        end,
       },
     }
   end,
