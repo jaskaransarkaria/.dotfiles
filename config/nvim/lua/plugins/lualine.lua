a5cb847c5248a00ee8200df0193e8bf238f93145
Jaskaran Sarkaria <jaskaran.sarkaria@googlemail.com>
2024-10-13 21:05:18 +0100

    refactor: 💡 remove unneccessary dep

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -1,8 +1,47 @@
 return {
   'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons', 'meuter/lualine-so-fancy.nvim' },
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    require('lualine').setup {
+    local lualine = require 'lualine'
+
+    local function show_macro_recording()
+      local recording_register = vim.fn.reg_recording()
+      if recording_register == '' then
+        return ''
+      else
+        return 'Recording @' .. recording_register
+      end
+    end
+
+    vim.api.nvim_create_autocmd('RecordingEnter', {
+      callback = function()
+        lualine.refresh {
+          place = { 'statusline' },
+        }
+      end,
+    })
+
+    vim.api.nvim_create_autocmd('RecordingLeave', {
+      callback = function()
+        -- This is going to seem really weird!
+        -- Instead of just calling refresh we need to wait a moment because of the nature of
+        -- `vim.fn.reg_recording`. If we tell lualine to refresh right now it actually will
+        -- still show a recording occurring because `vim.fn.reg_recording` hasn't emptied yet.
+        -- So what we need to do is wait a tiny amount of time (in this instance 50 ms) to
+        -- ensure `vim.fn.reg_recording` is purged before asking lualine to refresh.
+        local timer = vim.loop.new_timer()
+        timer:start(
+          50,
+          0,
+          vim.schedule_wrap(function()
+            lualine.refresh {
+              place = { 'statusline' },
+            }
+          end)
+        )
+      end,
+    })
+    lualine.setup {
       options = {
         icons_enabled = true,
         theme = 'palenight',
@@ -32,7 +71,10 @@ return {
           },
         },
         lualine_x = { 'fileformat', 'filetype' },
-        lualine_y = { 'progress', 'searchcount', 'fancy_macro' },
+        lualine_y = { 'progress', 'searchcount', {
+          'macro-recording',
+          fmt = show_macro_recording,
+        } },
         lualine_z = { 'location' },
       },
       inactive_sections = {
