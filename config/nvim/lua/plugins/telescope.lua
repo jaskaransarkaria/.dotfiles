c1a2723afc5f5ecd58ea679f6d9b9dc4948dc386
Jaskaran Sarkaria <jaskaran.sarkaria@googlemail.com>
2024-10-20 14:40:02 +0100

    fix: 🐛 conflicting cmdline and Ex commands

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -23,9 +23,7 @@ return {
     { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
     { 'jonarrien/telescope-cmdline.nvim' },
   },
-  keys = {
-    { ':', '<cmd>Telescope cmdline<cr>', desc = 'Cmdline' },
-  },
+  keys = {},
   opts = {
     extensions = {
       cmdline = {
@@ -101,7 +99,7 @@ return {
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-    vim.keymap.set('n', ':', ':Telescope cmdline<CR>', { noremap = true, desc = 'Cmdline' })
+    vim.keymap.set('n', 'Q', ':Telescope cmdline<CR>', { noremap = true, desc = 'Cmdline' })
 
     -- Slightly advanced example of overriding default behavior and theme
     vim.keymap.set('n', '<leader>/', function()
