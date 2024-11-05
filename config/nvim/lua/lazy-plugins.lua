1a0b7dc88208c89b49064e88b80660607ba6f005
Jaskaran Sarkaria <jaskaran.sarkaria@googlemail.com>
2024-11-05 11:47:18 +0000

    perf: ⚡️ handle big files

--- config/nvim/lua/lazy-plugins.lua
+++ config/nvim/lua/lazy-plugins.lua
@@ -33,6 +33,7 @@ require('lazy').setup {
   require 'plugins.bufferline',
   require 'plugins.dashboard',
   require 'plugins.harpoon',
+  require 'plugins.bigfiles',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
