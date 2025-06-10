3b83d57735ae9b323e9d73f00f99d42b20309eba
Jaskaran Sarkaria <jaskaran.sarkaria@googlemail.com>
2025-06-10 21:48:18 +0100

    fix: 🐛 btc price tracker

--- scripts/get-bitcoin-price.sh
+++ scripts/get-bitcoin-price.sh
@@ -1,1 +1,2 @@
-curl -s http://api.coindesk.com/v1/bpi/currentprice.json | python -c "import json, sys; print(json.load(sys.stdin)['bpi']['USD']['rate'])"
+# curl -s http://api.coindesk.com/v1/bpi/currentprice.json | python -c "import json, sys; print(json.load(sys.stdin)['bpi']['USD']['rate'])"
+curl -s rate.sx/1BTC | cut -d . -f 1 |rev| fold -w3 | paste -sd'.' - |rev
